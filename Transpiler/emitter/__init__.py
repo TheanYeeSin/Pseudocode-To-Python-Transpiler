73c3ba68d619c73f88cd5a520bc7bfbdf2333d10
Thean Yee Sin <yeesinthean@outlook.com>
2024-03-30 15:08:00 +0800

    Update Emitter

--- Transpiler/emitter/__init__.py
+++ Transpiler/emitter/__init__.py
@@ -1,4 +1,24 @@
-
-
 class Emitter:
-    pass
+    """Emitter that emit code in python."""
+    
+    def __init__(self, full_path: str) -> None:
+        self.full_path = full_path
+        self.header = ""
+        self.code = ""
+        
+    def emit(self, code: str) -> None:
+        """Emit new code."""
+        self.code += code
+        
+    def emit_line(self, code: str) -> None:
+        """Emit new code with new line."""
+        self.code += f"{code}\n"
+        
+    def header_line(self, code: str) -> None:
+        """Emit the header code."""
+        self.header += f"{code}\n"
+        
+    def write_file(self) -> None:
+        """Create a file contain python code."""
+        with open(self.full_path, 'w') as output_file:
+            output_file.write(self.header + self.code)
