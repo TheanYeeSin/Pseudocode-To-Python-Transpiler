4b1737bf51ea91803fffe0ecc3ce91395e2730d4
Thean Yee Sin <yeesinthean@outlook.com>
2024-03-30 16:09:15 +0800

    Update Transpiler

--- Transpiler/__init__.py
+++ Transpiler/__init__.py
@@ -2,3 +2,15 @@ from Transpiler.lexer import Lexer
 from Transpiler.token import Token, TokenTypes
 from Transpiler.parser import Parser
 from Transpiler.emitter import Emitter
+
+class Transpiler:
+    
+    def __init__(self, input: str, full_path: str) -> None:
+        self.lexer = Lexer(input)
+        self.emitter = Emitter(full_path)
+        self.parser = Parser(self.lexer, self.emitter)
+
+    def transpile(self) -> None:
+        self.parser.program() # Start the parser.
+        self.emitter.write_file() 
+        print("Parsing completed.")
