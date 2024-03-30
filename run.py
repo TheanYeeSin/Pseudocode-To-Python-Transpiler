bcfdcc5c48651f7a5b41a97002e6d80e69830255
Thean Yee Sin <yeesinthean@outlook.com>
2024-03-30 16:09:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 import sys
-from Transpiler import Lexer, Token, TokenTypes, Parser, Emitter
+from Transpiler import Transpiler
 
 if __name__ == "__main__":
-    pass
+    print("Transpiler")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as input_file:
+        input = input_file.read()
+
+    transpile = Transpiler(input=input, full_path="out.py")
+    
+    transpile.transpile()
+    
