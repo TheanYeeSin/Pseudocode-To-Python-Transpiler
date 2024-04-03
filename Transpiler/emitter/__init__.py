ed746f949adcf4dd7b37af2f48386adc66186f28
Thean Yee Sin <yeesinthean@outlook.com>
2024-04-03 18:45:09 +0800

    Update Emitter: handling indentation

--- Transpiler/emitter/__init__.py
+++ Transpiler/emitter/__init__.py
@@ -5,6 +5,7 @@ class Emitter:
         self.full_path = full_path
         self.header = ""
         self.code = ""
+        self.indent_count = 0
         
     def emit(self, code: str) -> None:
         """Emit new code."""
@@ -12,7 +13,12 @@ class Emitter:
         
     def emit_line(self, code: str) -> None:
         """Emit new code with new line."""
-        self.code += f"{code}\n"
+        self.code += f"{code}\n" + "\t" * self.indent_count
+        
+    def remove_indent(self) -> None:
+        """Remove indentation from code."""
+        if self.code.endswith("\t"):
+            self.code = self.code[:-1]
         
     def header_line(self, code: str) -> None:
         """Emit the header code."""
