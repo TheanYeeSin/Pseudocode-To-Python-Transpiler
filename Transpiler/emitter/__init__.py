dc83e99b3a7f5e707a5e5e7c578250b54d085f30
Thean Yee Sin <yeesinthean@outlook.com>
2024-04-07 13:27:25 +0800

    Update Emitter: Formatting

--- Transpiler/emitter/__init__.py
+++ Transpiler/emitter/__init__.py
@@ -1,30 +1,30 @@
 class Emitter:
     """Emitter that emit code in python."""
-    
+
     def __init__(self, full_path: str) -> None:
         self.full_path = full_path
         self.header = ""
         self.code = ""
         self.indent_count = 0
-        
+
     def emit(self, code: str) -> None:
         """Emit new code."""
         self.code += code
-        
+
     def emit_line(self, code: str) -> None:
         """Emit new code with new line."""
         self.code += f"{code}\n" + "\t" * self.indent_count
-        
+
     def remove_indent(self) -> None:
         """Remove indentation from code."""
         if self.code.endswith("\t"):
             self.code = self.code[:-1]
-        
+
     def header_line(self, code: str) -> None:
         """Emit the header code."""
         self.header += f"{code}\n"
-        
+
     def write_file(self) -> None:
         """Create a file contain python code."""
-        with open(self.full_path, 'w') as output_file:
+        with open(self.full_path, "w") as output_file:
             output_file.write(self.header + self.code)
